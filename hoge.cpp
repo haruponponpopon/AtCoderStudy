c04fa482dea0bcbae9a5852032ac1d64dbfbf9d0
haruponponpopon <kobayashi-haruka784@g.ecc.u-tokyo.ac.jp>
2021-06-26 14:38:35 +0900

    199.5C

--- hoge.cpp
+++ hoge.cpp
@@ -1,44 +1,32 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
+#include <climits>
+#include <numeric>
 using namespace std;
-
 int main(){
     int N;
-    long long total = 0;
     cin >> N;
-    vector<long long> A(N);
+    vector<vector<int>> ability(N, vector<int>(5));
+    vector<int> max_ability(5);
     for (int i=0; i<N; i++){
-        cin >> A.at(i);
-        total += A.at(i);
-    }
-    if (total%10){
-        cout << "No" << endl;
-        return 0;
-    }
-    long long piece = total/10;
-
-    int index1 = 0;
-    int index2 = 0;
-    long long cake = A.at(0);
-    while (index1<N){
-        // cout << index1 << " " << index2 << " " << cake << endl;
-        if (cake==piece){
-            cout << "Yes" << endl;
-            return 0;
-        }else if (cake>piece) {
-            cake -= A.at(index1);
-            if (index1!=index2) index1++;
-            else {
-                if (index2==N-1) index2 = -1;
-                index1++;
-                index2++;
-                if (index1<N) cake += A.at(index1);
-            }
-        }else{
-            if (index2==N-1) index2 = -1;
-            index2++;
-            cake += A.at(index2);
+        for (int j=0; j<5; j++){
+            cin >> ability.at(i).at(j);
+            max_ability.at(j) = max(ability.at(i).at(j), max_ability.at(j));
         }
     }
-    cout << "No" << endl;
+    int ans = 0;
+    for (int i=0; i<N-1; i++){
+        for (int j=i+1; j<N; j++){ //候補者i,j
+            vector<int> candidate(5);
+            for (int k=0; k<5; k++) candidate.at(k) = max(ability.at(i).at(k), ability.at(j).at(k));
+            vector<int> order(5);
+            iota(order.begin(), order.end(),0); 
+            sort(order.begin(), order.end(),[&](int x,int y) {
+                return candidate[x] > candidate[y];
+            });
+            ans = max(ans, min(candidate.at(order.at(3)), max_ability.at(order.at(4))));
+        }
+    }
+    cout << ans << endl;
 }
