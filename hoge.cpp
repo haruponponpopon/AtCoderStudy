8f282b3ab2800fa94076e62757b9ab9b544fc063
haruponponpopon <kobayashi-haruka784@g.ecc.u-tokyo.ac.jp>
2021-06-26 15:05:05 +0900

    199.5D

--- hoge.cpp
+++ hoge.cpp
@@ -1,32 +1,33 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
-#include <climits>
-#include <numeric>
+#include <deque>
+#include <string>
 using namespace std;
 int main(){
-    int N;
-    cin >> N;
-    vector<vector<int>> ability(N, vector<int>(5));
-    vector<int> max_ability(5);
-    for (int i=0; i<N; i++){
-        for (int j=0; j<5; j++){
-            cin >> ability.at(i).at(j);
-            max_ability.at(j) = max(ability.at(i).at(j), max_ability.at(j));
+    string S;
+    cin>>S;
+    int flag = 0;
+    deque<char> q;
+    for (int i=0; i<(int)S.size(); i++){
+        if (S[i]=='R') flag ^= 1;
+        else {
+            if (flag){
+                if (!q.empty() && S[i]==q.front()) q.pop_front();
+                else q.push_front(S[i]);
+            }else{
+                if (!q.empty() && S[i]==q.back()) q.pop_back();
+                else q.push_back(S[i]);
+            }
         }
     }
-    int ans = 0;
-    for (int i=0; i<N-1; i++){
-        for (int j=i+1; j<N; j++){ //候補者i,j
-            vector<int> candidate(5);
-            for (int k=0; k<5; k++) candidate.at(k) = max(ability.at(i).at(k), ability.at(j).at(k));
-            vector<int> order(5);
-            iota(order.begin(), order.end(),0); 
-            sort(order.begin(), order.end(),[&](int x,int y) {
-                return candidate[x] > candidate[y];
-            });
-            ans = max(ans, min(candidate.at(order.at(3)), max_ability.at(order.at(4))));
+    if (flag) {
+        while (!q.empty()){
+            cout << q.back();
+            q.pop_back();
+        }
+    }else{
+        while(!q.empty()){
+            cout << q.front();
+            q.pop_front();
         }
     }
-    cout << ans << endl;
 }
