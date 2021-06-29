3797c77aba72596dc303a4b343be77a560a195ba
haruponponpopon <kobayashi-haruka784@g.ecc.u-tokyo.ac.jp>
2021-06-30 08:29:17 +0900

    典型アルゴリズム問題集F(21/2/26)

--- fuga.cpp
+++ fuga.cpp
@@ -1,35 +1,45 @@
-#include <vector>
 #include <iostream>
-#include <queue>
-#include <tuple>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
+class UnionFind {
+public:
+	vector<int> par;
+
+	void init(int sz) {
+		par.resize(sz, -1);
+	}
+	int root(int pos) {
+		if (par[pos] == -1) return pos;
+		par[pos] = root(par[pos]);
+		return par[pos];
+	}
+	void unite(int u, int v) {
+		u = root(u); v = root(v);
+		if (u == v) return;
+		par[u] = v;
+	}
+	bool same(int u, int v) {
+		if (root(u) == root(v)) return true;
+		return false;
+	}
+};
+
 int main(){
-    int N,M,u,v,c,u1,c1;
+    int N,M;
     cin>>N>>M;
-    vector<vector<pair<int, int>>> city(N);
+    vector<vector<int>> c(M, vector<int>(3));
+    for (int i=0; i<M; i++) cin>>c.at(i).at(0)>>c.at(i).at(1)>>c.at(i).at(2);
+    sort(c.begin(),c.end(),[](const vector<int> &x,const vector<int> &y){return x[2] < y[2];});
+    UnionFind UF;
+    UF.init(N);
+    long long ans=0;
     for (int i=0; i<M; i++){
-        cin>>u>>v>>c;
-        city[u].push_back({v,c});
-        city[v].push_back({u,c});
-    }
-    priority_queue<pair<int,int>,vector<pair<int,int>>,greater<>> que;
-    long long ans = 0;
-    vector<int> used(N);
-
-    /*初期設定*/
-    que.emplace(0,0);
-
-    while(!que.empty()){
-        auto num = que.top(); que.pop();
-        tie(c,u) = num;
-        if (used.at(u)) continue;
-        used.at(u) = 1;
-        ans += c;
-        for (auto num1: city.at(u)){
-            tie(u1,c1) = num1;
-            if (!used.at(u1)) que.emplace(c1,u1);
+        if (!UF.same(c.at(i).at(0), c.at(i).at(1))){
+            ans += c.at(i).at(2);
+            UF.unite(c.at(i).at(0), c.at(i).at(1));
         }
     }
     cout << ans << endl;
-}
+}   
