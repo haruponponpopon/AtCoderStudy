d2916068e83067f0acf77ec32bd012b3b2b498bb
haruponponpopon <kobayashi-haruka784@g.ecc.u-tokyo.ac.jp>
2021-06-30 09:18:37 +0900

    ARC076D

--- fuga.cpp
+++ fuga.cpp
@@ -3,6 +3,12 @@
 #include <algorithm>
 using namespace std;
 
+struct Point{
+    int city1;
+    int city2;
+    int dis;
+};
+
 class UnionFind {
 public:
 	vector<int> par;
@@ -27,19 +33,38 @@ public:
 };
 
 int main(){
-    int N,M;
-    cin>>N>>M;
-    vector<vector<int>> c(M, vector<int>(3));
-    for (int i=0; i<M; i++) cin>>c.at(i).at(0)>>c.at(i).at(1)>>c.at(i).at(2);
-    sort(c.begin(),c.end(),[](const vector<int> &x,const vector<int> &y){return x[2] < y[2];});
+    int N;
+    cin>>N;
+    vector<vector<int>> x(N, vector<int>(2));
+    vector<vector<int>> y(N, vector<int>(2));
+
+    for (int i=0; i<N; i++){
+        cin>>x.at(i).at(0)>>y.at(i).at(0);
+        x.at(i).at(1) = i;
+        y.at(i).at(1) = i;
+    }
+
+    sort(x.begin(),x.end(),[](const vector<int> &x1,const vector<int> &y1){return x1[0] < y1[0];});
+    sort(y.begin(),y.end(),[](const vector<int> &x1,const vector<int> &y1){return x1[0] < y1[0];});
+
+    // for (int i=0; i<N; i++) cout << x.at(i).at(0) << " " << x.at(i).at(1) << endl;
+
+    vector<Point> distance;
+    for (int i=1; i<N; i++) {
+        distance.push_back({x.at(i-1).at(1), x.at(i).at(1), abs(x.at(i-1).at(0)-x.at(i).at(0))});
+        distance.push_back({y.at(i-1).at(1), y.at(i).at(1), abs(y.at(i-1).at(0)-y.at(i).at(0))});
+    }
+    sort(distance.begin(),distance.end(),[](const struct Point &x1,const struct Point &y1){return x1.dis < y1.dis;});
+
+    long long ans = 0;
+
     UnionFind UF;
     UF.init(N);
-    long long ans=0;
-    for (int i=0; i<M; i++){
-        if (!UF.same(c.at(i).at(0), c.at(i).at(1))){
-            ans += c.at(i).at(2);
-            UF.unite(c.at(i).at(0), c.at(i).at(1));
+    for (int i=0; i<(int)distance.size(); i++){
+        if (!UF.same(distance.at(i).city1, distance.at(i).city2)){
+            UF.unite(distance.at(i).city1, distance.at(i).city2);
+            ans += distance.at(i).dis;
         }
     }
     cout << ans << endl;
-}   
+}
