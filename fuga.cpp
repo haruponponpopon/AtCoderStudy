1121ad466ea7a225d1641f8a84e127bdf34fe06a
haruponponpopon <kobayashi-haruka784@g.ecc.u-tokyo.ac.jp>
2021-07-04 14:07:42 +0900

    典型79

--- fuga.cpp
+++ fuga.cpp
@@ -3,16 +3,48 @@
 using namespace std;
 
 int main(){
-    int N,M,a,b;
-    cin >> N >> M;
-    vector<int> node(N+1);
-    for (int i=0; i<M; i++){
-        cin >> a >> b;
-        node.at(max(a,b))++;
+    int H,W;
+    cin >> H >> W;
+    vector<vector<int>> A(H, vector<int>(W));
+    vector<vector<int>> B(H, vector<int>(W));
+    for (int i=0; i<H; i++){
+        for (int j=0; j<W; j++){
+            cin >> A.at(i).at(j);
+        }
     }
-    int ans=0;
-    for (int i=1; i<=N; i++){
-        if (node.at(i) == 1) ans++;
+    for (int i=0; i<H; i++){
+        for (int j=0; j<W; j++){
+            cin >> B.at(i).at(j);
+        }
     }
+
+    vector<vector<int>> gap(H, vector<int>(W));
+    for (int i=0; i<H; i++){
+        for (int j=0; j<W; j++){
+            gap.at(i).at(j) = B.at(i).at(j) - A.at(i).at(j);
+        }
+    }
+
+    vector<vector<int>> opr(H, vector<int>(W));
+    for (int i=0; i<H; i++){
+        for (int j=0; j<W; j++){
+            int index = 0;
+            if (i>0) index += opr.at(i-1).at(j);
+            if (j>0) index += opr.at(i).at(j-1);
+            if (i>0&&j>0) index += opr.at(i-1).at(j-1);
+            opr.at(i).at(j) = gap.at(i).at(j) - index;
+        }
+    }
+    long long ans = 0;
+    for (int i=0; i<H; i++){
+        for (int j=0; j<W; j++){
+            ans += abs(opr.at(i).at(j));
+            if ((i==H-1 || j==W-1)&&opr.at(i).at(j)){
+                cout << "No" << endl;
+                return 0;
+            }
+        }
+    }
+    cout << "Yes" << endl;
     cout << ans << endl;
 }
