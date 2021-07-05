dcc3d3895cc4d66e5ccadcdafd92c6679a3c1ce3
haruponponpopon <kobayashi-haruka784@g.ecc.u-tokyo.ac.jp>
2021-07-05 09:18:02 +0900

    ABC208D

--- hoge.cpp
+++ hoge.cpp
@@ -1,18 +1,27 @@
 #include <iostream>
-#include <cmath>
+#include <vector>
+#include <climits>
 using namespace std;
+
 int main(){
-    long long N;
-    cin>>N;
-    int ans = 1;
-    for (long long i=2; i<=sqrt(N); i++){
-        int count = 1;
-        while (N%i==0){
-            count++;
-            N /= i;
-        }
-        if (i!=2) ans *= count;
+    /*初期化*/
+    int N,M,A,B,C;
+    cin>>N>>M;
+    vector<vector<long long>> cost(N+1, vector<long long>(N+1, LLONG_MAX/2));
+    for (int i=1; i<=N; i++) cost.at(i).at(i) = 0;
+    for (int k=0; k<M; k++){
+        cin>>A>>B>>C;
+        cost.at(A).at(B) = C;
     }
-    if (N!=1) ans *= 2;
-    cout << 2*ans << endl;
+    /*ワーシャルフロイド*/
+    long long ans = 0;
+    for (int k=1; k<=N; k++){
+        for (int i=1; i<=N; i++){
+            for (int j=1; j<=N; j++){
+                cost.at(i).at(j) = min(cost.at(i).at(j), cost.at(i).at(k)+cost.at(k).at(j));
+                ans += (cost.at(i).at(j)==LLONG_MAX/2)?0:cost.at(i).at(j);
+            }
+        }
+    }
+    cout << ans << endl;
 }
