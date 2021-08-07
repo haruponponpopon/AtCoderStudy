60454ea5f3f6ca5a17280e866f0a628b7c0b662f
haruponponpopon <kobayashi-haruka784@g.ecc.u-tokyo.ac.jp>
2021-08-07 22:59:37 +0900

    ABC210F

--- hoge.cpp
+++ hoge.cpp
@@ -1,64 +1,24 @@
 #include <iostream>
 #include <vector>
-#include <climits>
+#include <algorithm>
 using namespace std;
 
+int gcd (int a,int b) {return (a%b ? gcd(b,a%b): b);}
+
 int main(){
-    int H,W;
-    long long A,C;
-    cin>>H>>W>>C;
-    vector<vector<long long>> A1(H, vector<long long>(W));
-    vector<vector<long long>> A2(H, vector<long long>(W));
-    for (int i=0; i<H; i++){
-        for (int j=0; j<W; j++){
-            cin>>A;
-            A1.at(i).at(j) = A;
-            A2.at(i).at(W-j-1) = A;
-        }
-    }
-    vector<vector<long long>> dp1(H, vector<long long>(W));
-    for (int i=0; i<H; i++){
-        for (int j=0; j<W; j++){
-            if (i==0){
-                if (j==0) dp1.at(i).at(j) = A1.at(i).at(j);
-                else dp1.at(i).at(j) = min(A1.at(i).at(j), dp1.at(i).at(j-1)+C);
-            }else if (j==0) dp1.at(i).at(j) = min(A1.at(i).at(j), dp1.at(i-1).at(j)+C);
-            else dp1.at(i).at(j) = min(min(A1.at(i).at(j), dp1.at(i-1).at(j)+C), dp1.at(i).at(j-1)+C);
-        }
-    }
-    long long ans = LLONG_MAX;
-    for (int i=0; i<H; i++){
-        for (int j=0; j<W; j++){
-            long long cur;
-            if (i==0){
-                if (j==0)continue;
-                else cur = dp1.at(i).at(j-1) + A1.at(i).at(j);
-            }else if (j==0)cur = dp1.at(i-1).at(j) + A1.at(i).at(j);
-            else cur = min(dp1.at(i-1).at(j), dp1.at(i).at(j-1))+A1.at(i).at(j);
-            ans = min(ans, cur+C);
-        }
-    }
-    //第二弾
-    vector<vector<long long>> dp2(H, vector<long long>(W));
-    for (int i=0; i<H; i++){
-        for (int j=0; j<W; j++){
-            if (i==0){
-                if (j==0) dp2.at(i).at(j) = A2.at(i).at(j);
-                else dp2.at(i).at(j) = min(A2.at(i).at(j), dp2.at(i).at(j-1)+C);
-            }else if (j==0) dp2.at(i).at(j) = min(A2.at(i).at(j), dp2.at(i-1).at(j)+C);
-            else dp2.at(i).at(j) = min(min(A2.at(i).at(j), dp2.at(i-1).at(j)+C), dp2.at(i).at(j-1)+C);
-        }
-    }
-    for (int i=0; i<H; i++){
-        for (int j=0; j<W; j++){
-            long long cur;
-            if (i==0){
-                if (j==0)continue;
-                else cur = dp2.at(i).at(j-1) + A2.at(i).at(j);
-            }else if (j==0)cur = dp2.at(i-1).at(j) + A2.at(i).at(j);
-            else cur = min(dp2.at(i-1).at(j), dp2.at(i).at(j-1))+A2.at(i).at(j);
-            ans = min(ans, cur+C);
-        }
-    }
-    cout << ans << endl;
+	int N,M;
+	cin>>N>>M;
+	vector<vector<int>> AC(M, vector<int>(2));
+	for (int i=0; i<M; i++) cin>>AC.at(i).at(0)>>AC.at(i).at(1);
+	sort(AC.begin(),AC.end(),[](const vector<int> &x,const vector<int> &y){return x[1] < y[1];});
+	int group = N;
+	long long ans = 0;
+	for (int i=0; i<M; i++){
+		int a = AC.at(i).at(0);//x
+		int c = AC.at(i).at(1);//cost
+		ans += 1LL*(group-gcd(group, a))*c;
+		group = gcd(group, a);
+	}
+	if (group==1) cout << ans << endl;
+	else cout << -1 << endl;
 }
