3f53b3c85d7442e7b18468b92589bd0123feda21
haruponponpopon <kobayashi-haruka784@g.ecc.u-tokyo.ac.jp>
2021-08-18 21:08:00 +0900

    ABC214D

--- hoge.cpp
+++ hoge.cpp
@@ -1,49 +1,53 @@
 #include <iostream>
 #include <vector>
-#include <string>
-#include <climits>
-#include <tuple>
-#include <deque>
+#include <algorithm>
 using namespace std;
+class UnionFind {
+public:
+	vector<int> par;
+	vector<int> group;
+
+	void init(int sz) {
+		par.resize(sz, -1);
+		group.resize(sz, 1);
+	}
+	int root(int pos) {
+		if (par[pos] == -1) return pos;
+		par[pos] = root(par[pos]);
+		return par[pos];
+	}
+	void unite(int u, int v) {
+		u = root(u); v = root(v);
+		if (u == v) return;
+		par[u] = v;
+		group[v] = group[u]+group[v];
+		// cout<<"group " << v << " "<< group[v]<<endl;
+	}
+	bool same(int u, int v) {
+		if (root(u) == root(v)) return true;
+		return false;
+	}
+	int group_size(int u) {
+		// cout << "u " << u  << " " << root(u) << " " << group[root(u)]<< endl;
+		return group[root(u)];
+	}
+};
+
 int main(){
-	int H,W;
-	cin>>H>>W;
-	vector<string> S(H);
-	for (auto &s: S) cin>>s;
-	vector<vector<int>> dist(H, vector<int>(W, INT_MAX));
-	deque<pair<int, int>> que;
-	vector<pair<int,int>> go = {{-1,0}, {0,-1}, {1,0}, {0, 1}};
-	vector<int> punch = {-2, -1, 0, 1, 2};
-	que.push_back({0,0});
-	dist.at(0).at(0) = 0;
-	while (que.size()){
-		auto current = que.front();que.pop_front();
-		int c_x = current.first;
-		int c_y = current.second;
-		/*パンチしないで進む*/
-		for (int i=0; i<4; i++){ 
-			int n_x = c_x+go.at(i).first;
-			int n_y = c_y+go.at(i).second;
-			if (n_x<0||n_x>=W||n_y<0||n_y>=H) continue; //領域外に出る
-			if (S.at(n_y).at(n_x)=='#') continue; //壁
-			if (dist.at(n_y).at(n_x)>dist.at(c_y).at(c_x)){ //距離が更新される
-				dist.at(n_y).at(n_x) = dist.at(c_y).at(c_x);
-				que.push_front({n_x, n_y});
-			}
-		}
-		/*パンチして進む*/
-		for (int i=0; i<5; i++){ 
-			for (int j=0; j<5; j++){
-				if ((i==0&&j==0)||(i==0&&j==4)||(i==4&&j==0)||(i==4&&j==4)) continue; //そもそも進めない
-				int n_x = c_x+punch.at(i);
-				int n_y = c_y+punch.at(j);
-				if (n_x<0||n_x>=W||n_y<0||n_y>=H) continue; //領域外に出る
-				if (dist.at(n_y).at(n_x)>dist.at(c_y).at(c_x)+1){ //距離が更新される
-					dist.at(n_y).at(n_x) = dist.at(c_y).at(c_x)+1;
-					que.push_back({n_x, n_y});
-				}
-			}
+	int N;
+	cin>>N;
+	vector<vector<int>> node(N-1, vector<int>(3));
+	UnionFind UF;
+	UF.init(N+1);
+	for (auto &n: node) cin>>n.at(0)>>n.at(1)>>n.at(2);
+	sort(node.begin(),node.end(),[](const vector<int> &x,const vector<int> &y){return x[2] < y[2];});
+	long long ans = 0;
+	for (auto &n: node){
+		if (UF.same(n.at(0), n.at(1))==false) {
+			ans += 1LL*UF.group_size(n.at(0))*UF.group_size(n.at(1))*n.at(2);
+			// cout << UF.group_size(n.at(0)) << " " << UF.group_size(n.at(1)) << endl;
+			UF.unite(n.at(0),n.at(1));
 		}
 	}
-	cout << dist.at(H-1).at(W-1) << endl;
+	cout<<ans<<endl;
 }
