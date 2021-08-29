a544572ffc9b94097dee9aacade232b3ac7f2cb8
haruponponpopon <kobayashi-haruka784@g.ecc.u-tokyo.ac.jp>
2021-08-29 16:00:10 +0900

    ABC197C

--- hoge.cpp
+++ hoge.cpp
@@ -1,32 +1,26 @@
 #include <iostream>
 #include <vector>
+#include <climits>
 using namespace std;
-void dfs(vector<int> &color, vector<bool> &used, vector<int> &good, vector<vector<int>> &graph, int current, vector<int> &C){
-    if (color.at(C.at(current))==0) good.at(current) = true;
-    color.at(C.at(current))++;
-    used.at(current) = true;
-    for (auto next: graph.at(current)){
-        if (used.at(next)==false){
-            dfs(color, used,good,graph,next,C);
-        }
-    }
-    color.at(C.at(current))--;
-}
 int main(){
-    int N,A,B;cin>>N;
-    vector<int>C(N+1);
-    for (int i=1; i<=N; i++)cin>>C.at(i);
-    vector<vector<int>> graph(N+1);
-    for (int i=0; i<N-1; i++){
-        cin>>A>>B;
-        graph[A].push_back(B);
-        graph[B].push_back(A);
-    }
-    vector<int>color(1e5+1);
-    vector<bool>used(N+1, false);
-    vector<int>good(N+1, false);
-    dfs(color,used,good,graph,1,C);
-    for (int i=1; i<=N; i++){
-        if (good.at(i))cout<<i<<endl;
-    }
+	int N;cin>>N;
+	vector<int>A(N);
+	for (auto &a: A)cin>>a;
+	int bit_m = 1<<(N-1);
+	int ans = INT_MAX;
+	for (int i=0; i<bit_m; i++){
+		int cur = 0;
+		int ins = 0;
+		for (int j=0; j<N-1; j++){
+			ins |= A.at(j);
+			if (1&(i>>j)){
+				cur ^= ins;
+				ins = 0;
+			}
+		}
+		ins |= A.at(N-1);
+		cur ^= ins;
+		ans = min(ans, cur);
+	}
+	cout<<ans<<endl;
 }
