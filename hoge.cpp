fc6c0a418959f1f59db09d2a2d0797aea4c53797
haruponponpopon <kobayashi-haruka784@g.ecc.u-tokyo.ac.jp>
2021-08-29 16:21:51 +0900

    ABC197D

--- hoge.cpp
+++ hoge.cpp
@@ -1,26 +1,14 @@
 #include <iostream>
-#include <vector>
-#include <climits>
+#include <complex>
 using namespace std;
 int main(){
-	int N;cin>>N;
-	vector<int>A(N);
-	for (auto &a: A)cin>>a;
-	int bit_m = 1<<(N-1);
-	int ans = INT_MAX;
-	for (int i=0; i<bit_m; i++){
-		int cur = 0;
-		int ins = 0;
-		for (int j=0; j<N-1; j++){
-			ins |= A.at(j);
-			if (1&(i>>j)){
-				cur ^= ins;
-				ins = 0;
-			}
-		}
-		ins |= A.at(N-1);
-		cur ^= ins;
-		ans = min(ans, cur);
-	}
-	cout<<ans<<endl;
+	int N;
+	double x0,y0,xh,yh;cin>>N>>x0>>y0>>xh>>yh;
+	complex<double> p0(x0,y0);
+	complex<double> ph(xh,yh);
+	complex<double> center = (p0+ph)/2.0;
+	complex<double> r = p0-center;
+	complex<double> rad = polar(1.0, 2*M_PI/N);
+	complex<double> ans = center + r*rad;
+	cout<<ans.real()<<" "<<ans.imag()<<endl;
 }
