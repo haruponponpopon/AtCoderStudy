1e49e0f55474990930598b72ad82783ed9a00d87
haruponponpopon <kobayashi-haruka784@g.ecc.u-tokyo.ac.jp>
2021-08-29 14:55:06 +0900

    ABC198E

--- hoge.cpp
+++ hoge.cpp
@@ -1,47 +1,32 @@
-#include <vector>
 #include <iostream>
-#include <string>
+#include <vector>
 using namespace std;
+void dfs(vector<int> &color, vector<bool> &used, vector<int> &good, vector<vector<int>> &graph, int current, vector<int> &C){
+    if (color.at(C.at(current))==0) good.at(current) = true;
+    color.at(C.at(current))++;
+    used.at(current) = true;
+    for (auto next: graph.at(current)){
+        if (used.at(next)==false){
+            dfs(color, used,good,graph,next,C);
+        }
+    }
+    color.at(C.at(current))--;
+}
 int main(){
-	int N;string S;cin>>N>>S;
-	int E = 998244353;
-	vector<vector<vector<int>>> dp(N+1, vector<vector<int>>(1024, vector<int>(10))); //i回目, パターン, 前回の参加
-
-	//bitDP
-	for (int i=1; i<=N; i++){ //i回目のコンテスト参加時点
-		//まず前回のコンテストのコピー
-		for (int j=0; j<1024; j++){
-			for (int k=0; k<10; k++){
-				dp.at(i).at(j).at(k) = dp.at(i-1).at(j).at(k);
-				/*前回の参加と今回のコンテストが一致したら2倍する
-				B未
-				BB 　　みたいに倍になるから*/
-				if (k==S[i-1]-'A') {
-					dp.at(i).at(j).at(k) += dp.at(i-1).at(j).at(k);
-					dp.at(i).at(j).at(k) %= E;
-				}
-			}
-		}
-		int c = S[i-1]-'A';
-		//今回のコンテストに、今まで参加していなかったら、その分を加算
-		for (int j=0; j<1024; j++){
-			if ((1&(j>>c))==0){
-				for (int k=0; k<10; k++){
-					dp.at(i).at(j+(1<<c)).at(c) += dp.at(i).at(j).at(k);
-					dp.at(i).at(j+(1<<c)).at(c) %= E;
-				}
-			}
-		}
-		//今回初参加
-		dp.at(i).at(1<<c).at(c)++;
-		dp.at(i).at(1<<c).at(c) %= E;
-	}
-	int ans = 0;
-	for (int i=0; i<1024; i++){
-		for (int j=0; j<10; j++){
-			ans += dp.at(N).at(i).at(j);
-			ans %= E;
-		}
-	}
-	cout<<ans<<endl;
+    int N,A,B;cin>>N;
+    vector<int>C(N+1);
+    for (int i=1; i<=N; i++)cin>>C.at(i);
+    vector<vector<int>> graph(N+1);
+    for (int i=0; i<N-1; i++){
+        cin>>A>>B;
+        graph[A].push_back(B);
+        graph[B].push_back(A);
+    }
+    vector<int>color(1e5+1);
+    vector<bool>used(N+1, false);
+    vector<int>good(N+1, false);
+    dfs(color,used,good,graph,1,C);
+    for (int i=1; i<=N; i++){
+        if (good.at(i))cout<<i<<endl;
+    }
 }
