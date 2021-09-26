c0c57c60ac04daa3ef2d986be3f1046a077bc765
haruponponpopon <kobayashi-haruka784@g.ecc.u-tokyo.ac.jp>
2021-09-26 15:05:18 +0900

    ABC192E

--- hoge.cpp
+++ hoge.cpp
@@ -1,34 +1,42 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
+#include <queue>
+#include <climits>
 using namespace std;
+struct node{
+	int go;
+	int k;
+	int time;
+};
 int main(){
-	int N;cin>>N;
-	vector<vector<long long>>CX(N, vector<long long>(2));
-	for (auto &cx: CX)cin>>cx.at(1)>>cx.at(0);
-	sort(CX.begin(),CX.end(),[](const vector<long long> &x,const vector<long long> &y){return x[0] < y[0];});
-	long long left = 0;//経路の最小
-	long long right = 0;//経路の最小
-	int index = 0;
-	long long mini_p = 0; //座標の最小
-	long long maxi_p = 0;//座標の最大
-	while (index<N){
-		long long mini = CX.at(index).at(1);
-		long long maxi = CX.at(index).at(1);
-		while (index+1<N&&CX.at(index).at(0)==CX.at(index+1).at(0)){
-			index++;
-			mini = min(mini,CX.at(index).at(1));
-			maxi = max(maxi,CX.at(index).at(1));
-		}
-		long long right_p = right;
-		long long left_p = left;
-		right = min(right_p+abs(maxi_p-mini), left_p+abs(mini_p-mini))+maxi-mini;
-		left = min(right_p+abs(maxi_p-maxi), left_p+abs(mini_p-maxi))+maxi-mini;
-
-		mini_p = mini;
-		maxi_p = maxi;
-		index++;
+	int N,M,X,Y,A,B,T,K;cin>>N>>M>>X>>Y;
+	vector<vector<node>> graph(N+1);
+	for (int i=0; i<M; i++){
+		cin>>A>>B>>T>>K;
+		graph[A].push_back({B,K,T});
+		graph[B].push_back({A,K,T});
 	}
-	long long ans = min(right+abs(maxi_p), left+abs(mini_p));
-	cout<<ans<<endl;
+	priority_queue<pair<long long, int>,vector<pair<long long, int>>,greater<>> que;
+	vector<bool>used(N+1, false);
+	vector<long long>time(N+1,LLONG_MAX);
+	que.push({0,X});
+	time.at(X) = 0;
+	while (!que.empty()){
+		auto current = que.top();que.pop();
+		if (time[current.second]<current.first) continue;
+		// cout<<current.second<<endl;
+		used[current.second]=true;
+		for (auto next: graph[current.second]){
+			if (used[next.go])continue;
+			long long arrive = current.first;
+			if (arrive%next.k!=0)arrive = (arrive/next.k+1)*next.k;
+			arrive += next.time;
+			// cout<<next.go<<" "<<arrive<<endl;
+			if (arrive<time[next.go]){
+				time[next.go] = arrive;
+				que.push({arrive, next.go});
+			}
+		}
+	}
+	if (time[Y]==LLONG_MAX)cout<<-1<<endl;
+	else cout<<time[Y]<<endl;
 }
