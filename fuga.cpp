564dfeb041b0e05d299e0eba14eb92444e1e94fa
haruponponpopon <kobayashi-haruka784@g.ecc.u-tokyo.ac.jp>
2021-09-26 12:32:37 +0900

    ABC192D

--- fuga.cpp
+++ fuga.cpp
@@ -1,48 +1,53 @@
 #include <iostream>
 #include <vector>
-#include <string>
-#include <numeric>
 #include <algorithm>
 using namespace std;
-long long convert_to_num(vector<int>& digit, string& S, vector<int>& abc){
-    //一番最初の桁が0だったらエラーの-1を返す。
-    if (digit.at(abc.at(S[0]-'a'))==0)return -1;
-    long long ans = 0;
-    for (auto s: S){
-        ans = ans*10 + digit.at(abc.at(s-'a'));
-    }
-    return ans;
+
+bool check(vector<long long> &x_arr, long long n, long long m){ //xのn進法はmより大きいか
+	vector<long long> m_arr;
+	while (m){
+		m_arr.push_back(m%n);
+		m/=n;
+	}
+	reverse(m_arr.begin(), m_arr.end());
+	if (m_arr.size()<x_arr.size())return true;
+	else if (m_arr.size()>x_arr.size()) return false;
+	for (size_t i=0; i<m_arr.size(); i++){
+		if (m_arr[i]<x_arr[i])return true;
+		else if (m_arr[i]>x_arr[i])return false;
+	}
+	return false;
 }
+
 int main(){
-    vector<string>S(3);
-    for (auto &s:S)cin>>s;
-    vector<int>abc(26,-1);
-    int count = 0;
-    for (int i=0; i<3; i++){
-        for (auto s: S[i]){
-            if (abc.at(s-'a')==-1){
-                abc.at(s-'a')=count;
-                count++;
-            }
-        }
-    }
-    if (count>10){
-        cout<<"UNSOLVABLE"<<endl;
-        return 0;
-    }
-    vector<int>digit(10);
-    iota(digit.begin(), digit.end(),0);
-    do {
-        vector<long long> N(3);
-        bool flag = true;
-        for (int i=0; i<3; i++){
-            N.at(i) = convert_to_num(digit,S.at(i),abc);
-            if (N.at(i)==-1)flag = false;
-        }
-        if (N.at(0)+N.at(1)==N.at(2)&&flag){
-            for (auto n: N)cout<<n<<endl;
-            return 0;
-        }
-    }while(next_permutation(digit.begin(), digit.end())); //vectorで書くならこう
-    cout<<"UNSOLVABLE"<<endl;
+	string x; long long m; cin>>x>>m;
+	long long d = 0;
+	vector<long long> arr;
+	for (char X: x){
+		d = max(d, 1LL*(X - '0'));
+		arr.push_back(1LL*(X - '0'));
+	}
+	if (x.size()==1){
+		if (x[0]-'0'>m)cout<<0<<endl;
+		else cout<<1<<endl;
+		return 0;
+	}
+	long long ok = d+1;
+	long long ng = (long long) 1e18+1;
+	// cout<<ng<<endl;
+	if (check(arr,ok,m)){
+		cout<<0<<endl;
+		return 0;
+	}
+	while (ng-ok>1){
+		long long mid = (ok+ng)/2;
+		// cout<<ok<<" "<<ng<<" "<<mid<<endl;
+		if (check(arr,mid,m)){
+			ng = mid;
+		}else{
+			ok = mid;
+		}
+	}
+	// cout<<ng<<" "<<d+1<<endl;
+	cout<<ng-(d+1)<<endl;
 }
